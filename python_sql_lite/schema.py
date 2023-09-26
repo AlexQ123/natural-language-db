13cc0a36ce7609f8653ee1200951d1885eb5c79c
stephenhood99 <st@hood.org>
2023-09-26 11:09:50 -0600

    fix schema

--- python_sql_lite/schema.py
+++ python_sql_lite/schema.py
@@ -1,51 +1,61 @@
-sql_create_category_table = """
-    CREATE TABLE categories (
-    category_id INT PRIMARY KEY,
-    category_name TEXT
-    );
+sql_create_users_table = """
+  CREATE TABLE users (
+    user_id TEXT PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    password TEXT
+  );
 """
 
-sql_create_menu_table = """
-    CREATE TABLE menu (
-        menu_id INT PRIMARY KEY ,
-        menu_name TEXT,
-        category_id INT,
-        unit_price REAL,
-        FOREIGN KEY (category_id) references categories(category_id)
-    );
+sql_create_videos_table = """
+  CREATE TABLE videos (
+    video_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id TEXT,
+    date DATETIME,
+    FOREIGN KEY (user_id) references users(user_id)
+  );
 """
 
-sql_create_customers_table = """
-    CREATE TABLE customers (
-    customer_id INT PRIMARY KEY ,
-    firstname TEXT,
-    Lastname TEXT,
-    city TEXT
-);
+sql_create_subscriptions_table = """
+  CREATE TABLE subscriptions (
+    subscription_id INT PRIMARY KEY AUTO_INCREMENT,
+    video_id INT PRIMARY KEY,
+    subscriber_id TEXT,
+    FOREIGN KEY (video_id) references videos(video_id),
+    FOREIGN KEY (subscriber_id) references users(user_id)
+  );
 """
 
-sql_create_employee_table = """
-    CREATE TABLE employee (
-        emp_id INT PRIMARY KEY ,
-        firstname TEXT,
-        Lastname TEXT,
-        hiredate TEXT,
-        branch TEXT );
+sql_create_views_table = """
+  CREATE TABLE views (
+    view_id INT PRIMARY KEY AUTO_INCREMENT,
+    video_id INT,
+    user_id TEXT,
+    FOREIGN KEY (video_id) references videos(video_id),
+    FOREIGN KEY (user_id) references users(user_id),
+  );
 """
 
-sql_create_orders_table = """
-    CREATE TABLE Orders(
-        orderid INT,
-        orderdate TEXT,
-        menu_id INT,
-        quantity INT DEFAULT 0,
-        customer_id INT,
-        delivery_platform TEXT,
-        emp_id INT,
-        FOREIGN KEY (menu_id) references menu(menu_id)
-        FOREIGN KEY (customer_id) references customers(customer_id)
-        FOREIGN KEY (emp_id) references employee(emp_id)
-    );
+sql_create_likes_table = """
+  CREATE TABLE likes (
+    like_id INT PRIMARY KEY AUTO_INCREMENT,
+    video_id INT,
+    user_id TEXT,
+    FOREIGN KEY (video_id) references videos(video_id),
+    FOREIGN KEY (user_id) references users(user_id),
+  );
+"""
+
+sql_create_comments_table = """
+  CREATE TABLE comments (
+    comment_id INT PRIMARY KEY AUTO_INCREMENT,
+    video_id INT,
+    user_id TEXT,
+    date DATETIME,
+    conent TEXT,
+    FOREIGN KEY (video_id) references videos(video_id),
+    FOREIGN KEY (user_id) references users(user_id),
+  );
 """
 
 def get_schema():
